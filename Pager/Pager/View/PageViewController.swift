b0387a22c65dec949cd5caa2477f238e1c97c171
Boyan Yankov <boyan.yankov@stanga.net>
2016-04-18 15:17:10 +0300

    updated `PageViewController.swift` - added stub conformance to `UIPageViewControllerDataSource`

--- Pager/Pager/View/PageViewController.swift
+++ Pager/Pager/View/PageViewController.swift
@@ -13,7 +13,7 @@ class PageViewController: UIPageViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.dataSource = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -31,5 +31,15 @@ class PageViewController: UIPageViewController {
         // Pass the selected object to the new view controller.
     }
     */
+}
 
+extension PageViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        return nil
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        return nil
+    }
 }
